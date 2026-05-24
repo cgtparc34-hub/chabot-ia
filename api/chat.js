2231df839657fc3b95a7f6ad7b2157ce57f481e2
cgtparc34-hub <cgtparc34@gmail.com>
2026-05-24 12:47:37 +0200

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,42 +1,5 @@
-export default async function handler(req, res) {
-  try {
-    const message = req.body.message;
-
-    if (!message) {
-      return res.status(400).json({ reply: "Message vide" });
-    }
-
-    const response = await fetch(
-      "https://router.huggingface.co/v1/chat/completions",
-      {
-        method: "POST",
-        headers: {
-          "Authorization": `Bearer ${process.env.HF_TOKEN}`,
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          model: "microsoft/DialoGPT-medium",
-          messages: [
-            { role: "user", content: message }
-          ]
-        })
-      }
-    );
-
-    const data = await response.json();
-
-    console.log(data);
-
-    const reply =
-      data?.choices?.[0]?.message?.content ||
-      data?.generated_text ||
-      data?.error ||
-      "Pas de réponse IA";
-
-    return res.status(200).json({ reply });
-
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ reply: "Erreur serveur" });
-  }
+export default function handler(req, res) {
+  res.status(200).json({
+    reply: "API Vercel fonctionne 👍"
+  });
 }
